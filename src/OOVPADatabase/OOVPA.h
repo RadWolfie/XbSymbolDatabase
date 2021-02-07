972c20d244fd6fa95162d545c5567b7c4890768f
RadWolfie <RadWolfie@users.noreply.github.com>
2021-02-07 06:12:16 -0600

    Update OOVPA.h

--- src/OOVPADatabase/OOVPA.h
+++ src/OOVPADatabase/OOVPA.h
@@ -155,7 +155,7 @@ typedef struct _LOOVPA {
 
 #define COUNTARGS(...) (sizeof((LOVP[]){__VA_ARGS__}) / sizeof(LOVP))
 // clang-format off
-#define OOVPA_SIG(...) MSVC_EXPAND(COUNTARGS(__VA_ARGS__)), { __VA_ARGS__ } }
+#define OOVPA_SIG(...) MSVC_EXPAND(COUNTARGS( __VA_ARGS__ )), { __VA_ARGS__ } }
 // clang-format on
 
 #define OOVPA_XREF_Test(Name, Version, XRefSaveIndex, XRefCount, DetectSelect) \
